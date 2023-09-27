4e42aa12d0e173435ce2ae0ccfe3a6887798c469
Gamaliel Adun <adungamaliel@gmail.com>
2023-09-27 21:41:46 +0100

    eslint fix

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,7 @@
 export default function returnHowManyArguments(...args) {
-    let noArgs = 0;
-    for (const arg of args) {
-        noArgs += 1;
-    }
-    return noArgs
+  let noArgs = 0;
+  for (const arg of args) {
+    noArgs += 1;
+  }
+  return noArgs;
 }
