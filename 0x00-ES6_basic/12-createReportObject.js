a0de386aa7fbbcf62116cc09fa404f86ddbde525
Gamaliel Adun <adungamaliel@gmail.com>
2023-09-29 12:11:56 +0100

    added 12

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  const allEmployees = { ...employeesList };
+
+  return {
+    allEmployees,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
