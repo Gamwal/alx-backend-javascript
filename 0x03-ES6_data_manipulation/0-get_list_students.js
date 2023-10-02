a50f5ecdad5daf8f934c3c6ef82d8aba1b7e9fd0
Gamaliel Adun <adungamaliel@gmail.com>
2023-10-02 18:26:41 +0100

    updated 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,8 @@
 export default function getListStudents() {
-  const ob1 = { id: 1, firstname: 'Guillaume', location: 'San Francisco' };
-  const ob2 = { id: 2, firstname: 'James', location: 'Columbia' };
-  const ob3 = { id: 5, firstname: 'Serena', location: 'San Francisco' };
-  return [ob1, ob2, ob3];
+  const students = [
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
+    { id: 5, firstname: 'Serena', location: 'San Francisco' },
+  ];
+  return students;
 }
