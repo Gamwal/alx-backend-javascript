2ed714a1d12cfacfa6d549bfc31b35ee06786bc5
Gamaliel Adun <adungamaliel@gmail.com>
2023-11-22 17:44:06 +0100

    added 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,12 @@
-console.log('Welcome to Holberton School, what is your name?');
+// 1-stdin.js
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (input) => {
-  console.log(`Your name is: ${input.toString()}`);
+  process.stdout.write(`Your name is: ${input.toString()}`);
+
+  if (!process.stdin.isTTY) {
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  }
   process.exit();
 });
