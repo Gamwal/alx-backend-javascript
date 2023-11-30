7f497ea268e21992f179ed16749156e0b281cdd2
Gamaliel Adun <adungamaliel@gmail.com>
2023-11-30 16:39:49 +0100

    added 0-promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,11 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     const response = true;
+    const error = 'Error';
     if (response) {
       resolve('True');
     } else {
-      reject('False');
+      reject(error);
     }
   });
 }
