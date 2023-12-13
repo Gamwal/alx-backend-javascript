ff8896f2019a84f1b4b5efb4d250d823db533fbf
Gamaliel Adun <adungamaliel@gmail.com>
2023-12-13 19:16:17 +0100

    added 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,6 +4,8 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
 
-  return Promise.all(promises)
-    .then((results) => results);
+  return Promise.all(promises.map((promise) => promise.then(
+    (value) => ({ status: 'resolved', value }),
+    (error) => ({ status: 'rejected', value: error }),
+  )));
 }
