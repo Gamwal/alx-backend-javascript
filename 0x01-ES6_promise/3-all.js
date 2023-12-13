0a81f7b018161f92eee4084a3681f7afe3045a74
Gamaliel Adun <adungamaliel@gmail.com>
2023-12-13 18:32:01 +0100

    added 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,18 +3,18 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const promises = [createUser(), uploadPhoto()];
 
-	Promise.all(promises)
+  Promise.all(promises)
     .then((results) => {
-			const user = results[0];
-			const photo = results[1];
+      const user = results[0];
+      const photo = results[1];
 
-      const firstName = user.firstName;
-      const lastName = user.lastName;
-			const userBody = photo.body;
+      const { firstName } = user;
+      const { lastName } = user;
+      const userBody = photo.body;
 
-			console.log(`${userBody} ${firstName} ${lastName}`);
+      console.log(`${userBody} ${firstName} ${lastName}`);
     })
-		.catch((error) => {
-			console.error('Signup system offline');
-		})
+    .catch(() => {
+      console.error('Signup system offline');
+    });
 }
