a64efca8d40f3e97273c256176ff40f79d72a792
Gamaliel Adun <adungamaliel@gmail.com>
2023-12-16 05:01:27 +0100

    added 7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-underscore-dangle */
+
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  toString() {
+    return `[object ${this.code}]`;
+  }
+}
