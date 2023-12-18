170be363d607e121b7413d586dea2f11a243f3f5
Gamaliel Adun <adungamaliel@gmail.com>
2023-12-18 17:36:02 +0100

    added 9

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,7 +1,7 @@
 export default function groceriesList() {
   const result = new Map();
 
-  result.set('Apple', 10);
+  result.set('Apples', 10);
   result.set('Tomatoes', 10);
   result.set('Pasta', 1);
   result.set('Rice', 1);
