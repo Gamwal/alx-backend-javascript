8ff4bf13d57e98c447a358606f9fb403cba9781f
Gamaliel Adun <adungamaliel@gmail.com>
2023-12-18 16:26:39 +0100

    updated 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const dv = new DataView(buffer);
+  if (position >= length || position < 0) {
+    return new Error('Position outside range');
+  }
   dv.setInt8(position, value);
+
   return dv;
 }
